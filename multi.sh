e05b9bfcc0a058ea6631fb30d717eebf71fe7345
Kanyerezi30 <kanyerezi30@gmail.com>
2022-04-13 12:41:10 +0300

    Implemented a fast approach

--- multi.sh
+++ multi.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 # A function that performs fastqc given a file containing path of the reads
-fastqc(){
-	for i in `cat $1`
-	do
-		fastqc $i
-	done
+quality(){
+	cat $1 | parallel fastqc
 }
 
-
-fastqc
+quality $@
